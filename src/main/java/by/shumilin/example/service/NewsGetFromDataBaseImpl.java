0f0d2cb3b4e53e4016deadf9847d99205d63192f
Sergej Shumilin <shumilin845@gmail.com>
2018-07-10 22:08:21 +0400

    added logger

--- src/main/java/by/shumilin/example/service/NewsGetFromDataBaseImpl.java
+++ src/main/java/by/shumilin/example/service/NewsGetFromDataBaseImpl.java
@@ -2,6 +2,7 @@ package by.shumilin.example.service;
 
 import by.shumilin.example.entity.News;
 import by.shumilin.example.repository.NewsRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,19 +11,21 @@ import java.util.List;
 @Component
 public class NewsGetFromDataBaseImpl implements NewsGetFromDataBase {
 
-
+private static final Logger LOG = Logger.getLogger(NewsGetFromDataBaseImpl.class);
     @Autowired
     NewsRepository newsRepository;
 
     @Override
     public List<News> getNews(long id) {
         List<News> news = newsRepository.getNews(id);
+        LOG.info("метод вызвался с id=" + id);
         return news;
     }
 
     @Override
     public List<News> getAllNews() {
         List<News> allNews = newsRepository.getAllNews();
+        LOG.info("метод вызвался без парамметров");
         return allNews;
     }
 }
--- src/main/java/by/shumilin/example/service/NewsSaveToDataBaseImpl.java
+++ src/main/java/by/shumilin/example/service/NewsSaveToDataBaseImpl.java
@@ -1,11 +1,14 @@
 package by.shumilin.example.service;
 
 import by.shumilin.example.repository.NewsRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NewsSaveToDataBaseImpl implements NewsSaveToDataBase {
+
+    private static final Logger LOG = Logger.getLogger(NewsSaveToDataBaseImpl.class);
 
     @Autowired
     NewsRepository newsRepository;
@@ -13,5 +16,6 @@ public class NewsSaveToDataBaseImpl implements NewsSaveToDataBase {
     @Override
     public void save(String title, String content) {
     newsRepository.save(title, content);
+     LOG.info("Метод вызван с title = " + title + "и content = " + content);
     }
 }
